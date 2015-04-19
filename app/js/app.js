caa9b014b604afd53c0ef52ddbfdf326bdd917e6
Rory Sterley <rorysterley@gmail.com>
2015-04-19 16:30:44 -0700

    More data binding with controllers

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,15 @@
 require('angular/angular');
 
 var app = angular.module('app', []);
+
 app.controller('FirstController', function($scope) {
   $scope.counter = 0;
   $scope.add = function(amount) { $scope.counter += amount; };
   $scope.subtract = function(amount) { $scope.counter -= amount; };
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = {
+    name: 'El estudiante muy mal!'
+  };
 });
