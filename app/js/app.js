433cd78a483a62dbdc0e7a3900cfaed209138e34
Rory Sterley <rorysterley@gmail.com>
2015-04-21 15:50:05 -0700

    Directives: 4 ways to make a directive

--- app/js/app.js
+++ app/js/app.js
@@ -4,34 +4,10 @@ require('angular/angular');
 
 var app = angular.module('myApp', []);
 
-// Note: The backend does not currently sport '/api/check/'
-app.directive('ensureUnique', ['$http', function($http) {
+app.directive('myDirective', function() {
   return {
-    require: 'ngModel',
-    link: function(scope, ele, attrs, c) {
-      scope.$watch(attrs.ngModel, function(n) {
-        if (!n) { return; }
-        $http({
-          method: 'POST',
-          url: '/api/check/' + attrs.ensureUnique,
-          data: {'field': attrs.ensureUnique}
-        }).success(function(data) {
-          c.$setValidity('unique', data.isUnique);
-        }).error(function(data) {
-          c.$setValidity('unique', false);
-        });
-      });
-    }
+    restrict: 'ECMA', // element (E), class (C), attribute (A), comment (M)
+    replace: true, // replace directive with what is in the template
+    template: '<a href="http://google.com">Click me to go to Google</a>'
   };
-}]);
-
-app.controller('signupController', ['$scope', function($scope) {
-  $scope.submitted = false;
-  $scope.signupForm = function() {
-    if ($scope.signup_form.$valid) {
-      // Submit as normal
-    } else {
-      $scope.signup_form.submitted = true;
-    }
-  };
-}]);
+});
