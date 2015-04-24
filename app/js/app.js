6faf5b8f3e566b1c1b7226387dd9830fce25afc3
Rory Sterley <rorysterley@gmail.com>
2015-04-24 10:34:10 -0700

    Directives: ng-app

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,9 @@ require('angular/angular');
 
 var app = angular.module('myApp', []);
 
-app.run(function($rootScope, $timeout) {
-  $timeout(function() {
-    $rootScope.imgSrc = 'http://google.com/images/srpr/logo11w.png';
-  }, 2000);
+app.run(function($rootScope) {
+  $rootScope.someProperty = 'hello computer';
+  $rootScope.someAction = function() {
+    $rootScope.someProperty = 'hello human';
+  };
 });
