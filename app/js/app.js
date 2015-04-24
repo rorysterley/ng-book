e0126570ffa34a24543eaeafe14db274f7a17932
Rory Sterley <rorysterley@gmail.com>
2015-04-24 13:20:37 -0700

    Directives: ng-repeat

--- app/js/app.js
+++ app/js/app.js
@@ -4,18 +4,9 @@ require('angular/angular');
 
 var app = angular.module('myApp', []);
 
-app.controller('SomeController', ['$scope', function($scope) {
-  // best practice, always use a model
-  $scope.someModel = {
-    someValue: 'hello computer'
-  };
-  $scope.someAction = function() {
-    $scope.someModel.someValue = 'hello human, from parent';
-  };
-}]);
-
-app.controller('ChildController', ['$scope', function($scope) {
-  $scope.childAction = function() {
-    $scope.someModel.someValue = 'hello human, from child';
-  };
-}]);
+app.controller('PeopleController', function($scope) {
+  $scope.people = [
+    {name: 'Ari', city: 'San Francisco'},
+    {name: 'Erik', city: 'Seattle'}
+  ];
+});
