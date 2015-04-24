4f5cde861b81c2fffd378affa62e9575f58f5799
Rory Sterley <rorysterley@gmail.com>
2015-04-24 09:48:46 -0700

    Directives: ng-src

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,6 @@ var app = angular.module('myApp', []);
 
 app.run(function($rootScope, $timeout) {
   $timeout(function() {
-    $rootScope.myHref = 'http://google.com';
+    $rootScope.imgSrc = 'http://google.com/images/srpr/logo11w.png';
   }, 2000);
 });
