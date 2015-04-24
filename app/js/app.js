90ab59f3d32303694e0b2e119f2e176c9e931eeb
Rory Sterley <rorysterley@gmail.com>
2015-04-24 09:10:11 -0700

    Directives: ng-disabled

--- app/js/app.js
+++ app/js/app.js
@@ -4,19 +4,9 @@ require('angular/angular');
 
 var app = angular.module('myApp', []);
 
-app.directive('myDirective', function() {
-  return {
-    restrict: 'A', // (A) attribute style is standard for custom elements
-    replace: true, // replace directive with what is in the template
-    scope: {
-      myUrl: '=someAttr', // = binding strategy
-      myLinkText: '@' // binding strategy
-    },
-    template: '' +
-      '<div>' +
-        '<label>My Url Field:</label>' +
-        '<input type="text" ng-model="myUrl" />' +
-        '<a href="{{ myUrl }}">{{ myLinkText }}</a>' +
-      '</div>'
-  };
+app.run(function($rootScope, $timeout) {
+  $rootScope.isDisabled = true;
+  $timeout(function() {
+    $rootScope.isDisabled = false;
+  }, 5000);
 });
