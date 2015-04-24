2f63bd793bdcfc308245096774f4675d51100fe5
Rory Sterley <rorysterley@gmail.com>
2015-04-24 12:51:15 -0700

    Directives: Nested $scope done right

--- app/js/app.js
+++ app/js/app.js
@@ -5,19 +5,17 @@ require('angular/angular');
 var app = angular.module('myApp', []);
 
 app.controller('SomeController', ['$scope', function($scope) {
-  // anti-pattern, bare value
-  $scope.someBareValue = 'hello computer';
-
-  // set actions on $scope itself, this is okay
+  // best practice, always use a model
+  $scope.someModel = {
+    someValue: 'hello computer'
+  };
   $scope.someAction = function() {
-    // sets {{ someBareValue }} inside SomeController and ChildController
-    $scope.someBareValue = 'hello human, from parent';
+    $scope.someModel.someValue = 'hello human, from parent';
   };
 }]);
 
 app.controller('ChildController', ['$scope', function($scope) {
   $scope.childAction = function() {
-    // sets {{ someBareValue }} inside Childcontroller
-    $scope.someBareValue = 'hello human, from child';
+    $scope.someModel.someValue = 'hello human, from child';
   };
 }]);
