a986144da6c3e8f93397fe1ab3d0003f04f28394
Rory Sterley <rorysterley@gmail.com>
2015-04-24 10:46:32 -0700

    Directives: ng-controller

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,16 @@ require('angular/angular');
 
 var app = angular.module('myApp', []);
 
-app.run(function($rootScope) {
-  $rootScope.someProperty = 'hello computer';
-  $rootScope.someAction = function() {
-    $rootScope.someProperty = 'hello human';
+app.controller('SomeController', ['$scope', function($scope) {
+
+  // create a model
+  $scope.someModel = {
+    // with a property
+    someProperty: 'wow sutch property'
   };
-});
+
+  //set actions on $scope itself
+  $scope.someAction = function() {
+    $scope.someModel.someProperty = 'wow sutch response';
+  };
+}]);
